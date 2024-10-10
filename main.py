1caecefbde588522439eb142dc1130ada774d1ef
Jaime Garcia <74214287+JaimeJGarcia@users.noreply.github.com>
2024-10-09 23:04:42 -0700

    D: add game clock (fps)

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Initialize FPS game object
+    game_clock = pygame.time.Clock()
+    # delta time
+    dt = 0
+
+
+    # Game loop
     while True:
+        # interupt event from game loop to exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
+        # fill the game screen
+        screen.fill("black")
+        # re-draw game screen
         pygame.display.flip()
+        # get delta time and limit fps
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
